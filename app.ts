362a4023c76711e3bf58c7870ec27791c986b848
Ernesto Ramírez <ernestorb_@outlook.com>
2023-05-21 16:26:10 -0600

    Añadir endpoint /autores & habilitar CORS

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import routes from './routing'
+import cors from 'cors'
 const app = express()
 const port = Number(process.env.PORT ?? 4000)
 
@@ -9,6 +10,8 @@ if (Number.isNaN(app)) {
   process.exit(1)
 }
 
+app.use(cors({ origin: ['http://localhost:4200', 'cine.ernestorb.com'] }))
+app.use(express.static('static'))
 app.use(routes)
 
 app.listen(port, () => {
---
+++ models/Autor.ts
@@ -0,0 +1,6 @@
+export interface Autor {
+  nombre: string
+  apellido: string
+  image: string
+  id: number
+}
