ee220ef5430b1e95a64d2fe8b6dc8893594a61c3
Ernesto Ramírez <ernestorb_@outlook.com>
2023-05-20 23:55:16 -0600

    Identaciones de 2 espacios y no incluir hostname

--- app.ts
+++ app.ts
@@ -2,16 +2,15 @@ import express from 'express'
 import routes from './routing'
 const app = express()
 const port = Number(process.env.PORT ?? 4000)
-const hostname = process.env.HOSTNAME ?? '127.0.0.1'
 
 if (Number.isNaN(app)) {
-    console.error('Puerto inválido')
+  console.error('Puerto inválido')
 
-    process.exit(1)
+  process.exit(1)
 }
 
 app.use(routes)
 
-app.listen(port, hostname, () => {
-    console.log(`Escuchando en puerto ${port}`)
+app.listen(port, () => {
+  console.log(`Escuchando en puerto ${port}`)
 })
